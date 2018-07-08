9466c55748e9d232bdcbe3e90381bf546c5e9340
Simo Antikainen <simo.antikainen@aalto.fi>
2018-07-08 16:50:49 +0300

    part 5.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      user: null,
+      username: '',
+      password: '',
+      error: null
     }
   }
 
@@ -14,17 +19,77 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
-  } 
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+  
+      this.setState({ username: '', password: '', user})
+    } catch(exception) {
+      this.setState({
+        error: 'käyttäjätunnus tai salasana virheellinen',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+  
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
   render() {
+
+    const loginForm = () => (
+      <div>
+        <h2>Kirjaudu</h2>
+
+        <form onSubmit={this.login}>
+          <div>
+            käyttäjätunnus
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <div>
+            salasana
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <button>kirjaudu</button>
+        </form>
+      </div>
+    )
+
+    if (this.state.user === null) {
+      return (
+        <div>
+          {loginForm()} 
+        </div>
+      )
+    }
+  
     return (
       <div>
         <h2>blogs</h2>
-        {this.state.blogs.map(blog => 
-          <Blog key={blog._id} blog={blog}/>
+        {this.state.blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
         )}
       </div>
-    );
+    )
   }
 }
 
