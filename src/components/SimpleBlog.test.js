0151957bd470276783259799f87267d3014c50dc
Simo Antikainen <simo.antikainen@aalto.fi>
2018-07-13 18:51:50 +0300

    part 5.13

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import SimpleBlog from './SimpleBlog'
 
-describe.only('SimpleBlog />', () => {
+describe('SimpleBlog />', () => {
   it('renders content', () => {
     const blog = {
       author: 'Tim Urban',
@@ -17,4 +17,24 @@ describe.only('SimpleBlog />', () => {
     expect(contentDiv.text()).toContain(blog.title)
     expect(contentDiv.text()).toContain(blog.likes)
   })
+
+  it('when like is pressed twice the event handler tied to it is called twice', () => {
+
+    const blog = {
+      author: 'Tim Urban',
+      title: 'How to Pick a Career (That Actually Fits You)',
+      likes: 10
+    }
+
+    const mockHandler = jest.fn()
+    
+    const blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler}/>)
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+    expect(mockHandler.mock.calls.length).toBe(2)
+
+
+  })
+
 })
