b4f6109b4f8bd79272bad141b0ea12418b8ac508
Simo Antikainen <simo.antikainen@aalto.fi>
2018-07-21 23:55:30 +0300

    part 5.16

--- src/App.test.js
+++ src/App.test.js
@@ -30,63 +30,19 @@ describe('<App />', () => {
 
   describe('when user is logged', () => {
     beforeEach(() => {
-      //localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
       app = mount(<App />)
+      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
 
     })
 
-    it('all notes are rendered', () => {
+    it('all blogs are rendered when user logged on', () => {
       app.update()
-
-      //console.log("dsad",localStorage.getItem('loggedBlogAppUser'))
-
-      //app.instance().forceUpdate()
-      //app.update()
-      console.log(app.html())
+      //console.log(app.html())
 
       const blogComponents = app.find(Blog)
       expect(blogComponents.length).toEqual(blogService.blogs.length)
 
-
-
     })
   })
-
-
-
-  /**it('before log on no blogs are rendered and login form is rendered', () => {
-    app.update() //Forces a re-render
-    const blogComponents = app.find(Blog)
-    expect(blogComponents.length).toEqual(0)
-
-    expect(app.html()).toContain('login')
-    
-    //console.log(loginComponent.text())
-    //expect(blogComponents.length).toEqual(blogService.blogs.length)
-  })
-
-  it('after log on the blogs are rendered', () => {
-    app.update() //Forces a re-render
-
-    const user = {
-      username: 'tester',
-      token: '1231231214',
-      name: 'Teuvo Testaaja'
-    }
-    
-    localStorage.setItem('loggedBlogUser', JSON.stringify(user))
-
-    app.update()
-
-    console.log(app.html())
-
-    const blogComponents = app.find(Blog)
-    expect(blogComponents.length).toEqual(blogService.blogs.length)
-
-    //expect(app.html()).toContain('login')
-    
-    //console.log(loginComponent.text())
-    //expect(blogComponents.length).toEqual(blogService.blogs.length)
-  })**/
 
 })
